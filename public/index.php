e593c07b9bbdf60b5e86687b3fe284a4f77f2e45
dustin-chan-89 <busterdust@gmail.com>
2016-02-02 14:37:41 -0800

    changed index.php for heroku

--- public/index.php
+++ public/index.php
@@ -16,4 +16,25 @@ if (!is_file($path))
 	exit('Could not find your craft/ folder. Please ensure that <strong><code>$craftPath</code></strong> is set correctly in '.__FILE__);
 }
 
+// Check the SERVER_NAME variable ourselves
+switch ($_SERVER['SERVER_NAME'])
+{
+    // If the SERVER_NAME variable matches our case,
+    // assign the CRAFT_ENVIRONMENT variable a keyword
+    // that identifies this environment that we can
+    // use in our multi-environment config
+
+    case 'site.com' :
+        define('CRAFT_ENVIRONMENT', 'live');
+        break;
+
+    case 'staging.site.com' :
+        define('CRAFT_ENVIRONMENT', 'staging');
+        break;
+
+    default :
+        define('CRAFT_ENVIRONMENT', 'dev');
+        break;
+}
+
 require_once $path;
