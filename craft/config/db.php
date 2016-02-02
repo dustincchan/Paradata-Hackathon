68996197efbfd070bfb6ba2dc591b6b82f544857
dustin-chan-89 <busterdust@gmail.com>
2016-02-02 00:10:36 -0800

    Added Procfile, and composer

--- craft/config/db.php
+++ craft/config/db.php
@@ -10,19 +10,10 @@
 
 return array(
 
-	// The database server name or IP address. Usually this is 'localhost' or '127.0.0.1'.
-	'server' => 'localhost',
-
-	// The name of the database to select.
-	'database' => 'paradata',
-
-	// The database username to connect with.
-	'user' => 'root',
-
-	// The database password to connect with.
-	'password' => 'root',
-
-	// The prefix to use when naming tables. This can be no more than 5 characters.
-	'tablePrefix' => 'craft',
+	‘server’ => $url[“host”],
+	‘user’ => $url[“user”],
+	‘password’ => $url[“pass”],
+	‘database’ => substr($url[“path”],1),
+	‘tablePrefix’ => ‘craft’,
 
 );
